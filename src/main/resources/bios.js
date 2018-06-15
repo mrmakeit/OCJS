3cb9f8351b6ac505d296e92d2499c3a1f7b6fc4e
hasezoey <10911626+hasezoey@users.noreply.github.com>
2018-06-16 00:25:28 +0200

    make it better. (Tested & Working in ES5)

--- src/main/resources/bios.js
+++ src/main/resources/bios.js
@@ -1,74 +1,110 @@
-function decodeRead(arr){
-	var string = "";
-	for(var x in arr){
-		string = string + String.fromCharCode(arr[x]);
-	}
-	return string;
-}
+/* This is the non-JSDOC Version */
+var _computer = Object(computer);
 
-function getComponentList(type){
-	var allComp = computer.list();
-	var results = [];
-	for(var comp in allComp){
-		if(allComp[comp] == type){
-			results.push(comp);
-		}
-	}
-	return results;
-}
-
-function loadFrom(address, success){
-  computer.invoke(address, 'open', ['/init.js'],function(handle){
-    if(!handle){
-      return;
+function getCompList(type) {
+    var allComp = computer.list();
+    var results = [];
+    for (var comp in allComp) {
+        if (allComp[comp] == type) {
+            results.push(comp);
+        }
     }
-    var buffer = "";
-    var readData = function(results){ 
-      if(results){
-        var data = decodeRead(results);
-        buffer = buffer + data;
-        computer.invoke(address,"read",[handle, Number.MAX_VALUE],readData,function(error){});
-      }else{
-        computer.invoke(address,'close', [handle], function(){
-          success();
-          eval(buffer); 
-        }, function(){});
-      }
+    return results;
+}
+
+function setScreens(cb, addr) {
+    if (typeof addr != 'string') addr = null;
+    var gpu = getCompList('gpu')[0];
+    function set() {
+        if (!addr) var addr = getCompList('screen')[0];
+        computer.invoke(gpu, 'bind', [addr], function (_) {
+            cb();
+        }, function (err) {
+            cb(err);
+        });
     }
-    computer.invoke(address,"read",[handle, Number.MAX_VALUE],readData,function(error){});
-  },function(error){
-  });
-}
-
-function setScreens(){
-	var screen = getComponentList('screen')[0];
-	var gpu = getComponentList('gpu')[0];
-	if(screen){
-		if(gpu){
-      computer.invoke(gpu, 'bind', [screen],function(_){},function(_){});
-		}
-	}
-}
-
-function init(){
-  if(!runOne){
-    if(ready){
-    }else{
-      computer.error("No Bootable Medium Found");
+    if (addr) set();
+    else {
+        computer.invoke(gpu, 'getScreen', [], function (address) {
+            if (!address || address.trim() == '') set();
+            else cb();
+        }, function (err) {
+            cb(err);
+        });
     }
-  }
-  runOne = false;
-	setScreens();
-	var drives = getComponentList('filesystem')
-	if(drives.length>0){
-		for(var drive in drives){
-			var address = drives[drive];
-			loadFrom(address,function(){
-      })
-		}
-	}
 }
 
-var runOne = true;
-var ready = false;
-init();
+function scaninit(cb) {
+    var drives = getCompList('filesystem');
+    if (drives.length <= 0) computer.error('no filesystems installed!');
+    var index = -1;
+    saw(function (cb) {
+        index++;
+        if (index <= drives.length - 1) {
+            computer.invoke(drives[index], 'exists', ["/init.js"], function (b) { //clear lowest line
+                if (b) {
+                    cb(false, drives[index]);
+                } else cb(true);
+            }, function (_) {
+                computer.print('somehow something errored in write');
+            });
+        } else cb(false, null);
+    }, function (back) {
+        if (!back) {
+            computer.error('No init.js found!');
+        }
+
+        cb(back);
+    });
+}
+
+function saw(func, end) {
+    func(function (again, back) {
+        if (again) {
+            saw(func, end);
+        } else end(back);
+    });
+}
+
+function decodeRead(arr) {
+    var string = "";
+    for (var x in arr) {
+        string += String.fromCharCode(arr[x]);
+    }
+    return string;
+}
+
+setScreens(function (err) {
+    if (err) computer.error(err);
+    var gpu = getCompList('gpu');
+    computer.invoke(gpu, 'getResolution', [], function (x, y) {
+        computer.invoke(gpu, 'setBackground', [0], function () {
+            computer.invoke(gpu, 'fill', [1, 1, x, y, " "], function () {
+                scaninit(function (addr) {
+                    computer.invoke(addr, 'open', ['/init.js'], function (handle) {
+                        var buffer = '';
+                        function readData(results) {
+                            if (results) {
+                                buffer += decodeRead(results);
+                                computer.invoke(addr, "read", [handle, Number.MAX_VALUE], readData, function (err) { });
+                            } else {
+                                computer.invoke(addr, 'close', [handle], function () {
+                                    eval(buffer);
+                                }, function () { });
+                            }
+                        }
+                        computer.invoke(addr, "read", [handle, Number.MAX_VALUE], readData, function (err) { });
+                    }, function () {
+                        computer.print('Cloud not open "init.js" on ' + addr);
+                    });
+                });
+            }, function () {
+                computer.print('Something went wrong at clearing the screen!');
+            });
+        }, function () {
+            computer.print('Something went wrong at setting the background-color');
+        });
+    }, function () {
+        computer.print('Something went wrong at getting the Resolution!');
+    });
+});
